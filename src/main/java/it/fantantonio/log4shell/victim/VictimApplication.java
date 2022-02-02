7b5fa57b1da40767ea9c498f9e6607b2a3b5582c
Fantantonio <antoniopanfili@gmail.com>
2022-02-02 18:10:15 -0500

    Init commit

---
+++ src/main/java/it/fantantonio/log4shell/victim/VictimApplication.java
@@ -0,0 +1,13 @@
+package it.fantantonio.log4shell.victim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VictimApplication.class, args);
+    }
+
+}
